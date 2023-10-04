8168ea28722919e075ddbaada0929f306c249886
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-04 13:49:23 -0400

    Update response.php

--- response.php
+++ response.php
@@ -7,7 +7,7 @@
         <p><?= var_dump($_GET) ?></p>
         <p><?= var_dump($_POST) ?></p>
         <p>Hello your name is: <?=$_POST["firstname"] . " " . $_POST["lastname"]?> </p>
-        <p>You are a <?=$_POST["age"]. " year old " $_POST["gender"] ?> </p>
+        <p>You are a <?=$_POST["age"]. " year old " . $_POST["gender"] ?> </p>
         <p>Your email is <?=$_POST["email"]." and your phone number is: " . $_POST["phone"] ?>
         </body>
 </html>
