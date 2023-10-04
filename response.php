3e93bd4719bcbf3ece1d919c18213f0622f6b260
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-04 13:45:03 -0400

    added email and phone

--- response.php
+++ response.php
@@ -8,5 +8,6 @@
         <p><?= var_dump($_POST) ?></p>
         <p>Hello your name is: <?=$_POST["firstname"] . " " . $_POST["lastname"]?> </p>
         <p>You are <?=$_POST["age"] ?> years old </p>
+        <p>Your email is <?=$_POST["email"]."and your phone number is:" . $_POST["phone"] ?> 
         </body>
 </html>
