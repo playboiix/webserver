ebd7b10c1f50d8256b2c8b35f0dbbd7f7e2f844c
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-04 14:06:50 -0400

    Update response.php

--- response.php
+++ response.php
@@ -4,8 +4,6 @@
             <title>Sauce Village</title>
         </head>
         <body bgcolor="#696969">
-        <p><?= var_dump($_GET) ?></p>
-        
         <p>Hello your name is: <?=htmlspecialchars($_POST["firstname"]) . " " . htmlspecialchars($_POST["lastname"])?> </p>
         <p>You are a <?=(int)$_POST["age"]. " year old " . $_POST["gender"] ?> </p>
         <p>Your email is <?=$_POST["email"]." and your phone number is: " . $_POST["phone"] ?>
