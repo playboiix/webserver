411ef198c357ea4c5e5292365b77818564c972a8
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-04 12:58:56 -0400

    Update response.php

--- response.php
+++ response.php
@@ -4,6 +4,7 @@
             <title>Sauce Village</title>
         </head>
         <body bgcolor="#696969">
+        <p><?= var_dump($_GET) ?></p>
         <p><?= var_dump($_POST) ?></p>
         </body>
 </html>
