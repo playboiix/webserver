b0a387328fa4f46acc31f838cd11208c97c77b4d
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-18 15:05:18 -0400

    Update response2.php

--- response2.php
+++ response2.php
@@ -3,19 +3,19 @@
         <head>
             <title>Sauce Village</title>
             <?php
-            $id= htmlspecialchars($_GET["Students"]);
+            $gender= int($_GET["gender"]);
             $server = "localhost";
             $username = "sal";
             $password = "spiderman";
             $database = "DawsonCollege";
             $conn = mysqli_connect($server,$username,$password,$database);  
-            $sql = "select * from Students;";
+            $sql = "select * from Students where id={$gender};";
             $result = mysqli_query($conn, $sql);
             ?>
         </head>
         <body bgcolor="#6A5ACD">
         <p><?= var_dump($_GET) ?></p>
-        You selected <?= $id ?>.<br/>
+        You selected <?= $gender ?>.<br/>
         <?php
             foreach($result as $row)
             {
