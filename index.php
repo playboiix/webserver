a8b29e973c778d569628572ac1a527a348843b87
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-10-18 13:27:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,18 @@
             }
         </script>
 
+        <?php
+            $server = "localhost";
+            $username = "sal";
+            $password = "spiderman";
+            $database = "DawsonCollege";
+            $conn = mysqli_connect($server,$username,$password,$database);
+            if (!$conn) 
+            {die("Connection failed: {mysqli_connect_error()}");}
+            echo "Connected successfully";              
+            $sql = "select * from Students;";
+            $result = mysqli_query($conn, $sql);
+        ?>
     </head>
 
     <body bgcolor="#CCCCFF">
