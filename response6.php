1c493e1d097a749dbba9dd7571d7abbc170277a2
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-11-15 15:19:42 -0500

    Update response6.php

--- response6.php
+++ response6.php
@@ -9,7 +9,6 @@
     </head>
     <body bgcolor="#696969">
     <p>The sensor you are using is <?=$deserialized["sensor"]?></p>
-    <p>The humidity is <?=$deserialized["humidity"]?></p>
     <p>The pressure is <?=$deserialized["pressure"]?></p>
     <p>The temperature is <?=$deserialized["temperature"]?></p>
     <p>The altitude is <?=$deserialized["altitude"]?></p>
