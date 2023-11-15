c07d7ad57d6df541fc29432268a5385e0a737241
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2023-11-15 15:12:02 -0500

    Update response6.php

--- response6.php
+++ response6.php
@@ -8,5 +8,5 @@
         <title>Sauce Village</title>
     </head>
     <body bgcolor="#696969">
-    <p><?=`echo $deserialized["temperature"]`;?></p>
+    <p><?=$deserialized["temperature"];?></p>
 </html>
