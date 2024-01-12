c0bce3929aed079611415967c74ea03e1757431d
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2024-01-12 11:32:24 -0500

    Update search.php

--- finalwebserver/search.php
+++ finalwebserver/search.php
@@ -3,27 +3,22 @@
         <head>
             <?php
             $search= htmlspecialchars($_GET["search"]);
+            $ip= htmlspecialchars($_GET["ip"]);
             $_SERVER['REMOTE_ADDR'];
             $server = "localhost";
             $username = "sal";
             $password = "spiderman";
             $database = "DawsonCollege";
             $conn = mysqli_connect($server,$username,$password,$database);  
-            $sql = "insert into Students (gender, avg, age) values ('$gender', $avg, $age)";
+            $sql = "insert into Logs (search, ip) values ('$search', $ip)";
             $result = mysqli_query($conn, $sql);
             ?>
         </head>
         <body bgcolor="#6A5ACD">
-        <?php
-          echo "You have successfully created a student.</br>";
-          echo "Here are all the students below:</br></br>";
-          $sql = "SELECT * FROM Students;";
-          $result = mysqli_query($conn, $sql);
-            foreach($result as $row)
-            {
-                echo "A {$row['gender']} who is {$row['age']} years old and has an average of {$row['avg']}.</br>";
-            }
+            <?php
+            header('Location: https://google.com/search?q='$search);
+            $result = mysqli_query($conn, $sql);
             mysqli_close($conn);
-        ?>
+            ?>
         </body>
 </html>
