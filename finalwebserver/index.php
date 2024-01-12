3ebd94c4950f5358b18894fcc300bab2483d361e
Sal Usman <salman-ali.usman@dawsoncollege.qc.ca>
2024-01-12 10:52:44 -0500

    final

---
+++ finalwebserver/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $server = "localhost";
+            $username = "sal";
+            $password = "spiderman";
+            $database = "DawsonCollege";
+            $conn = mysqli_connect($server,$username,$password,$database);
+            // if (!$conn) 
+            // {die("Connection failed: {mysqli_connect_error()}");}
+            // echo "Connected successfully";              
+            $sql = "select * from Students;";
+            $result = mysqli_query($conn, $sql);
+        ?>
+    </head>
+    <body>
+        <center>
+            <img id="img" src="google.png"/><br/>
+        </center>
+</html>
